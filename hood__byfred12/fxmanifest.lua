bb03da186a223b3e3a1fdf8b5e6e26f6b71df81b
fredmapping <fredforget34@gmail.com>
2025-09-03 22:14:29 -0400

    Add files via upload

--- hood__byfred12/fxmanifest.lua
+++ hood__byfred12/fxmanifest.lua
@@ -1,7 +1,18 @@
 fx_version 'cerulean'
-games { 'rdr3', 'gta5' }
+game 'gta5'
 
-author 'FRED'
-description 'mapping_by_FRED'
-version '1.0.0'
+-- Déclare que c’est un mapping
+this_is_a_map 'yes'
 
+-- Liste tous les fichiers à charger
+files {
+    'stream/**/*.ymap',
+    'stream/**/*.ytyp',
+    'stream/**/*.ydr',
+    'stream/**/*.ytd'
+}
+
+-- Important : charger les YTYP
+-- ⚠️ Remplace les noms par ceux de tes fichiers réels
+data_file 'DLC_ITYP_REQUEST' 'stream/batiment.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/autre_batiment.ytyp'
